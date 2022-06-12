d1e91ce3b61c7ef7d35f753beca226b68efa4329
nicolascrr <77678413+nicolascrr@users.noreply.github.com>
2022-06-12 18:40:30 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 200,
+//     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, h3',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .pres__text, .veilletech__text, .arrow__left, .arrow__right, .cards-wrapper',{delay: 400});
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .pdfone, .contact__input, .contact__button, .gradient-list, .container',{interval: 200}); 
+
